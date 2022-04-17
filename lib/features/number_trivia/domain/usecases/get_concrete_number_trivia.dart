b265ab90612e9740122e0d42e78cfd3b2a873401
Фаиз <galimow63@gmail.com>
2022-04-17 19:21:45 +0400

    call() method implementing

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,15 +1,29 @@
-import 'package:dartz/dartz.dart';
-import 'package:number_trivia/core/errors/failures.dart';
-import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
-import '../repositories/number_trivia_repository.dart';
+import 'package:number_trivia/core/usecases/usecase.dart';
 
-class GetConcreteNumberTrivia {
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
+import '../../../../core/errors/failures.dart';
+
+import 'package:dartz/dartz.dart';
+
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failure, NumberTrivia>> call({@required int number}) async {
-    return await repository.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  const Params({@required this.number});
+
+  @override
+  List<Object> get props => [number];
 }
