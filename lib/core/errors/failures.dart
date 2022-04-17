3c0009df66ed8642a0db0167723098cadcd1d3d1
Фаиз <galimow63@gmail.com>
2022-04-17 14:37:26 +0400

    abstract Failure class added

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+// ignore: must_be_immutable
+abstract class Failure extends Equatable {
+  List properties = const <dynamic>[];
+
+  Failure(this.properties);
+
+  @override
+  List<Object?> get props => [properties];
+}
