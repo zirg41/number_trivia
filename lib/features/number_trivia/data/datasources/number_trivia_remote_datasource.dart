21bf4dcde64d948b730b193c839657c53b97ff7c
Фаиз <galimow63@gmail.com>
2022-04-19 19:23:08 +0400

    abstract class NumberTriviaRemoteDatasource added

---
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart
@@ -0,0 +1,13 @@
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+abstract class NumberTriviaRemoteDatasource {
+  /// Calls the http://numbersapi.com/{number} endpoint.
+  ///
+  /// Throws a [ServerException] for all error codes.
+  Future<NumberTrivia> getConcreteNumberTrivia(int number);
+
+  /// Calls the http://numbersapi.com/random endpoint.
+  ///
+  /// Throws a [ServerException] for all error codes.
+  Future<NumberTrivia> getRandomNumberTrivia();
+}
