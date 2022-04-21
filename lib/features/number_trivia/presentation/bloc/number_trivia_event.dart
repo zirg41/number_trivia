aff9d611f7994b22f680dc5d764bc34caf43e0e3
Фаиз <galimow63@gmail.com>
2022-04-21 14:13:07 +0400

    bloc files added

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -0,0 +1,23 @@
+part of 'number_trivia_bloc.dart';
+
+abstract class NumberTriviaEvent extends Equatable {
+  const NumberTriviaEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  // not int because:
+  //  1. the data from UI is gonna be a string
+  //  2. it's not proper to do converting data stuff
+  //     in presentation layer
+  final String numberString;
+
+  const GetTriviaForConcreteNumber(this.numberString);
+
+  @override
+  List<Object> get props => [numberString];
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {}
