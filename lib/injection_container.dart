63864dcd9a2a95b9f7142eb6444d403de693ef91
Фаиз <galimow63@gmail.com>
2022-04-21 22:09:35 +0400

    syntax error fixed in IC

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -52,6 +52,6 @@ Future<void> init() async {
   //! External
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton(() => sharedPreferences);
-  sl.registerLazySingleton(() => http.Client);
+  sl.registerLazySingleton(() => http.Client());
   sl.registerLazySingleton(() => DataConnectionChecker());
 }
