71208ac667657dfef08e71c624c5aa417ccb17f0
Фаиз <galimow63@gmail.com>
2022-04-21 23:11:53 +0400

    TriviaContols and trivia getting is implemented

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -42,24 +42,68 @@ class NumberTriviaPage extends StatelessWidget {
 
               const SizedBox(height: 10),
               //Botton half
-              Column(
-                children: [
-                  //Text field
-                  const Placeholder(fallbackHeight: 50),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: const [
-                      Expanded(child: Placeholder(fallbackHeight: 30)),
-                      SizedBox(width: 10),
-                      Expanded(child: Placeholder(fallbackHeight: 30)),
-                    ],
-                  )
-                ],
-              )
+              TriviaContols()
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class TriviaContols extends StatefulWidget {
+  const TriviaContols({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  State<TriviaContols> createState() => _TriviaContolsState();
+}
+
+class _TriviaContolsState extends State<TriviaContols> {
+  final controller = TextEditingController();
+  String inputString;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //Text field
+        TextField(
+          controller: controller,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+              border: OutlineInputBorder(), hintText: "Input a number"),
+          onChanged: (value) {
+            inputString = value;
+          },
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              child: RaisedButton(
+                color: Theme.of(context).primaryColor,
+                textTheme: ButtonTextTheme.primary,
+                child: const Text("Search"),
+                onPressed: dispatchConcrete,
+              ),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: RaisedButton(
+                child: const Text("Get random trivia"),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  void dispatchConcrete() {
+    controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context)
+        .add(GetTriviaForConcreteNumber(inputString));
   }
 }
